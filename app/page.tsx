f0c6f683d2cdac8d2552f1257de7b2f2689a00ac
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2024-08-20 18:38:01 +0500

    Base routes completed

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="h1-bold">Hello world!</h1>
-      <button className="btn primary-gradient px-5 py-5">Click here</button>
-    </main>
-  );
-}
