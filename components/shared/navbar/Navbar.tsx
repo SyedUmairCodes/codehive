5d81ad7834875a7893aee13e0e451c0442018784
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2024-08-24 10:06:07 +0500

    Navbar created

---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src={"/assets/icons/logo.png"}
+          alt="site-logo"
+          width={25}
+          height={25}
+        />
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Code<span className="text-primary-500">Hive</span>
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton
+            afterSwitchSessionUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#FACC15",
+              },
+            }}
+          />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
